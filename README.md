90c164c86c6a62b59b18471c74c12a05d3e2a11b
Quincy Bowers <qbowers@clearwateranalytics.com>
2014-04-01 10:50:00 -0600

    Finished implementing the initial version.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 wordsearch
 ==========
+A simple Web app to do regular expression searches on a dictioary file.
+
+The dictionary file is expected to be a file named "words" in your home
+directory.
+
+Installing
+----------
+    $ go get github.com/quincy/wordsearch
+    $ go install github.com/quincy/wordsearch
+
--- main.go
+++ main.go
@@ -3,27 +3,97 @@
 package main
 
 import (
+	"bufio"
 	"html/template"
 	"net/http"
+	"net/url"
+	"os"
 	"os/exec"
+	"os/user"
+	"path"
 	"path/filepath"
+	"regexp"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
 // Page represents a single page in the wiki.
 type Page struct {
 	Title   string
-	Matches []template.HTML
+	Query   string
+	Matches []string
+	Min     int
+	Max     int
+	Count   int
 }
 
-func createPage(pattern string) *Page {
-	return new(Page)
+func createPage(pattern string, min, max int) *Page {
+	matches, count := getMatches(pattern, min, max)
+	page := Page{
+		Title:   "wordsearcher",
+		Query:   pattern,
+		Matches: matches,
+		Min:     min,
+		Max:     max,
+		Count:   count}
+
+	return &page
+}
+
+func getMatches(query string, min, max int) (matches []string, count int) {
+	if query == "" {
+		query = ".*"
+	}
+
+	pattern := regexp.MustCompile(query)
+
+	for _, word := range words {
+		if min > 0 && len(word) < min {
+			continue
+		}
+		if max > 0 && len(word) > max {
+			continue
+		}
+		if pattern.MatchString(word) {
+			matches = append(matches, word)
+		}
+	}
+
+	return matches, len(matches)
 }
 
 // viewHandler prepares the page to be rendered by passing it through the
 // markdown and wikiMarkup filters.
-func searchHandler(w http.ResponseWriter, r *http.Request, pattern string) {
-	p := createPage(pattern)
+func searchHandler(w http.ResponseWriter, r *http.Request, params url.Values) {
+	var query string
+	var min int
+	var max int
+	var err error
+
+	if _, exists := params["query"]; !exists {
+		query = ""
+	} else {
+		query = params["query"][0]
+	}
+	if _, exists := params["min"]; !exists {
+		min = 0
+	} else {
+		min, err = strconv.Atoi(params["min"][0])
+		if err != nil {
+			panic(err)
+		}
+	}
+	if _, exists := params["max"]; !exists {
+		max = 0
+	} else {
+		max, err = strconv.Atoi(params["max"][0])
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	p := createPage(query, min, max)
 	renderTemplate(w, "view", p)
 }
 
@@ -41,9 +111,56 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+func makeHandler(fn func(http.ResponseWriter, *http.Request, url.Values)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fn(w, r, r.URL.Path)
+		fn(w, r, r.URL.Query())
+	}
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+var words []string
+
+func init() {
+	currentUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	dictionary := path.Join(currentUser.HomeDir, "words")
+	dict, err := readLines(dictionary)
+	if err != nil {
+		panic(err)
+	}
+
+	var wordMap map[string]bool = make(map[string]bool)
+
+	for _, line := range dict {
+		word := strings.Trim(line, "\n")
+
+		if strings.IndexAny(word, "'ABCDEFGHIJKLMNOPQRSTUVWXYZ") >= 0 {
+			continue
+		}
+
+		if _, exists := wordMap[word]; !exists {
+			wordMap[word] = true
+			words = append(words, word)
+		}
 	}
 }
 
@@ -68,5 +185,6 @@ func main() {
 
 	// register the handlers and start the server.
 	http.HandleFunc("/", makeHandler(searchHandler))
+	http.HandleFunc("/search", makeHandler(searchHandler))
 	http.ListenAndServe(server, nil)
 }
